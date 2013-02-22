ffc7042658071f0220bd9faeef465f893cce8f9e
James A. Robinson <jim.robinson@stanford.edu>
2013-02-22 07:24:52 -0800

    added TestLexerSkipPast

--- lex_test.go
+++ lex_test.go
@@ -24,9 +24,29 @@ func TestLexerAcceptRunA(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	item := l.NextItem()
 	if len(item.Value) != 10 {
-		t.Error("expected 10 bytes, got %d\n", len(item.Value))
+		t.Errorf("expected 10 bytes, got %d\n", len(item.Value))
+	}
+}
+
+func TestLexerSkipPast(t *testing.T) {
+	r := strings.NewReader("b\n\n\n\n\na")
+	l, err := NewLexer("TestLexerSkipPast", r, aRecord)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	item := l.NextItem()
+	if item.Type != ItemError {
+		t.Fatalf("expected ItemError on character b, got %q", item)
+	}
+
+	item = l.NextItem()
+	if item.Type != ItemEmit {
+		t.Fatalf("expected ItemEmit on character b, got %q", item)
+	}
+	if item.Value != "a" {
+		t.Fatalf("expected ItemEmit of one character 'a', got %q", item.Value)
 	}
 }
