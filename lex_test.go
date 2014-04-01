c99e07f4eb6755ab0bca0def32fad588bb5bf714
James A. Robinson <jim.robinson@stanford.edu>
2014-03-31 22:55:00 -0700

    commented out stub of a new test

--- lex_test.go
+++ lex_test.go
@@ -1,14 +1,77 @@
 package lexrec
 
 import (
+	//"fmt"
 	"strings"
 	"testing"
 )
 
 const (
 	ItemIgnore ItemType = ItemEOF + 1 + iota
+	ItemAorB
+	ItemTab
+	ItemA
+	ItemColon
+	ItemB
 	ItemEmit
 )
+
+type lexTest struct {
+	name   string
+	input  string
+	record Record
+	expect []Item
+}
+
+/*
+var (
+	tItemEOF = Item{ItemEOF, 0, ""}
+)
+
+var parseA = Record{Buflen:1, ErrorFn:SkipPast("\n"), States:[]Binding{{ItemA, AcceptRun("a", true),true}}}
+
+var lexTests = []lexTest{
+	{"empty", "", parseA, []Item[tItemEOF]},
+}
+
+func collect(l *Lexer) []Item {
+	items := []Item{}
+	for {
+		item := l.NextItem()
+		items = append(items, items)
+		if item.Type == ItemEOF {
+			break
+		}
+	}
+	return items
+}
+
+func TestLex(t *testing.T) {
+	for _, test := range lexTests {
+		l := NewLexer(test.name, test.record)
+		items := collect(l)
+
+		n := len(test.expect)
+		if len(items) < n {
+			t.Errorf("missing trailing items: %v", test.expect[len(items):])
+		} else if len(items) > n {
+			t.Errorf("unexpected extra items: %v", test.items[n:])
+		}
+
+		for i := range items {
+			if items[i].Type != test.expect[i].Type {
+				t.Errorf("got Type %q expected Type %q\n", items[i].Type, test.expect[i].Type)
+			}
+			if items[i].Pos != test.expect[i].Pos {
+				t.Errorf("got Pos %q expected Pos %q\n", items[i].Pos, test.expect[i].Pos)
+			}
+			if items[i].Value != test.expect[i].Value {
+				t.Errorf("got Value %q expected Value %q\n", items[i].Value, test.expect[i].Value)
+			}
+		}
+	}
+}
+*/
 
 var acceptRunA = AcceptRun("a", true)
 
@@ -31,7 +94,7 @@ func TestLexerAcceptRunA(t *testing.T) {
 }
 
 func TestLexerSkipPast(t *testing.T) {
-	r := strings.NewReader("b\n\n\n\n\na")
+	r := strings.NewReader("bbb\n\n\n\n\na")
 	l, err := NewLexer("TestLexerSkipPast", r, aRecord)
 	if err != nil {
 		t.Fatal(err)
